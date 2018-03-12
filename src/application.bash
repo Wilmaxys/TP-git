c59b21b92e657d0f68a1982c8545270accb75e5f
Benjamin Ragot <benjaminragot53000@gmail.com>
2018-03-12 15:51:39 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,6 @@
 ### application.bash - 2018 - Benjamin Ragot ###
-echo "Copyright company - 2018"
-echo 'Build: 10:00"
-echo "version 1.1"
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
